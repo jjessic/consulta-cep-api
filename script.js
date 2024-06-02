a183b161662745f9413ac887eab23bb9c9c35547
Jéssica <160645996+jjessic@users.noreply.github.com>
2024-06-02 18:28:11 +0000

    Atualização do Cadastro de CEP

--- script.js
+++ script.js
@@ -64,3 +64,87 @@ function limpaForm() {
 document.querySelectorAll('input:not(#cep)').forEach(input => {
     input.value = '';
 })}
+
+function cadastraCEP(enderecoCompleto) {
+    
+    fetch('http://localhost:3000/endereco', {
+        "method": "POST",
+        "headers": {
+            "Content-type": "application/json"
+        },
+        "body": JSON.stringify(enderecoCompleto)
+    }).then(resposta => {
+        // if (resposta.ok) window.alert('Endereço cadastrado')
+        // else window.alert('Erro: '+resposta.status)
+
+        resposta.ok ? window.alert('Endereço cadastrado!') :
+        window.alert('Erro: ' + resposta.status)
+    })
+
+}
+
+function atualizaCEP(id, enderecoCompleto) {
+    fetch(`http://localhost:3000/endereco/${id}`, {
+        "method": "PUT",
+        "headers": {
+            "Content-type": "application/json"
+        },
+        "body": JSON.stringify(enderecoCompleto)
+    }).then(resposta => {
+        resposta.ok ? window.alert('Endereço atualizado!') :
+        window.alert('Erro ' + resposta.status)
+    }).catch(error => {
+        window.alert('Erro na atualização ' + error.message);
+    });
+}
+
+function consultaPorBairro() {
+    const uf = document.querySelector('#uf').value;
+    const cidade = document.querySelector('#cidade').value;
+    const bairro = document.querySelector('#bairro').value;
+
+    if (bairro.length < 3) {
+        window.alert('O bairro deve conter pelo menos 3 caracteres');
+        return;
+    }
+
+    if (cidade.length < 3) {
+        window.alert('A cidade deve conter pelo menos 3 caracteres');
+        return;
+    }
+
+    const requisicao = new Request(`https://viacep.com.br/ws/${uf}/${cidade}/${bairro}/json`, {
+        "method": "GET",
+        "headers": {
+            "Content-type": "application/json"
+        }
+    });
+
+    fetch(requisicao)
+        .then(resposta => {
+            if (resposta.status === 400) {
+                throw new Error('Bad Request');
+            }
+            return resposta.json();
+        })
+        .then(resposta => {
+            if (resposta.length > 0) {
+                const resultado = resposta[0]; // Pega o primeiro resultado
+
+                document.querySelector('#logradouro').value = resultado.logradouro;
+                document.querySelector('#bairro').value = resultado.bairro;
+                document.querySelector('#cidade').value = resultado.localidade;
+                document.querySelector('#uf').value = resultado.uf;
+            } else {
+                limpaForm();
+                window.alert('Endereço não localizado');
+            }
+        })
+        .catch(error => {
+            limpaForm();
+            window.alert('Erro na consulta: ' + error.message);
+        });
+}
+
+document.querySelector('#btnBuscarEndereco').addEventListener('click', consultaPorEndereco);
+document.querySelector('#btnBuscarBairro').addEventListener('click', consultaPorBairro);
